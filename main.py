1c9ea3dd7531d2706d5fa077f2efa4410b9d7acc
Un Couteau <5453712-un_couteau@users.noreply.gitlab.com>
2022-11-17 21:38:02 +0500

    v2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from math import sqrt
+from typing import NoReturn
 
 
-def work7__1_2():
+def work7__1() -> NoReturn:
     a, b, c = map(int, input("Введите три числа через пробел: ").split())
-    D = b ** 2 - 4 * a * c
+    D: int = b ** 2 - 4 * a * c
 
     if not a and not b:
         print("Неразрешимое уравнение")
@@ -12,20 +13,22 @@ def work7__1_2():
     elif D < 0:
         print("Случай комплексных корней")
     elif not b and not c:
-        print("Нулевые корни: x1 = x2 = 0")
+        print("Нулевые корни: x1 = 0\n x2 = 0")
     elif D > 0:
-        x1 = (-b + sqrt(D)) / (2 * a)
-        x2 = (-b - sqrt(D)) / (2 * a)
-        print(f"x1: {x1}\nx2: {x2}")
+        print(f"x1: {(-b + sqrt(D)) / (2 * a)}\nx2: {(-b - sqrt(D)) / (2 * a)}")
     elif not D:
-        x = -b / 2 * a
+        print(f"x: {-b / 2 * a}")
 
 
-def work7__2():
+def work7__2() -> NoReturn:
+    work7__1()
+
+
+def work7__3() -> NoReturn:
     s1, s2 = map(str, input("Введите две строки через пробел: ").split())
 
-    count = 0
-    ind = 1
+    count: int = 0
+    ind: int = 1
     while ind != -1:
         ind = s1.find(s2)
         if ind >= 0:
@@ -34,12 +37,72 @@ def work7__2():
     print(f"Вывод: {str(count)}")
 
 
-def work8__1_2():
-    ...
+def work8__1() -> NoReturn:
+    square = {'A': 11, 'B': 12, 'C': 13, 'D': 14, 'E': 15,
+              'F': 21, 'G': 22, 'H': 23, 'I': 24, 'K': 25,
+              'L': 31, 'M': 32, 'N': 33, 'O': 34, 'P': 35,
+              'Q': 41, 'R': 42, 'S': 43, 'T': 44, 'U': 45,
+              'V': 51, 'W': 52, 'X': 53, 'Y': 54, 'Z': 55,
+              }
+    vr: str = input("Что вы хотите сделать?\n1) Зашифровать\n2) Расшифровать\nВаш выбор: ")
+
+    match vr:
+        case "1":
+            strng: str = input("Введите строчку: ").upper()
+
+            mess: str = ''
+            list_mess: list = list(strng)
+            for i in strng:
+                if i in square:
+                    mess += str(square.get(i, 0))
+                elif i not in square:
+                    print('Введите корректные данные')
+                    break
+                else:
+                    mess += (i + i)
+
+            with open(".last string", "w") as file:
+                file.write(mess)
+                file.close()
+            print("Шифр записан в файл \".last string\"")
+        case "2":
+            with open(".last string", "r") as file:
+                intgr: str = file.read()
+                file.close()
+
+            if not intgr.isnumeric():
+                print("Введите числа!")
+                quit()
+            else:
+                ss: str = ''
+                list_ss: list = []
+                step: int = 2
+                for i in range(0, len(intgr), 2):
+                    list_ss.append(intgr[i:step])
+                    step += 2
+                list_ss: list = list(map(int, list_ss))
+                key_square: list = list(square.keys())
+                val_square: list = list(square.values())
+
+                for i in list_ss:
+                    if i in val_square:
+                        i = val_square.index(i)
+                        ss += key_square[i]
+                    elif i not in val_square:
+                        print("Введите числа!")
+                    else:
+                        ss += i[0:1]
+                print(ss)
+        case _:
+            print("Ошибка!")
 
 
-def work8__2():
-    ...
+def work8__2() -> NoReturn:
+    work8__1()
+
+
+def work8__3() -> NoReturn:
+    print("Будет в отчете")
 
 
 if __name__ == '__main__':
@@ -50,12 +113,12 @@ if __name__ == '__main__':
 
             match work:
                 case "1":
-                    work7__1_2()
+                    work7__1()
                 case "2":
-                    work7__1_2()
-                case "3":
                     work7__2()
+                case "3":
+                    work7__3()
                 case _:
                     print("Нет такого варианта")
         case "8":
-            ...
+            work8__1()
